363c12253315e621eea2afdf5dd689c09c97b560
Dam Nguyen Khang <nguyeenkhang15112004@gmail.com>
2025-03-07 22:28:50 +0700

    add service

---
+++ src/main/java/dao/BaseDAO.java
@@ -0,0 +1,16 @@
+package dao;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public abstract class BaseDAO<T> {
+    static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("FruitManagementPU");
+    public abstract List<T> getAll();
+    public abstract T findById(int id);
+    public abstract void insert(T t) ;
+    public abstract boolean update(T t) ;
+    public abstract boolean delete(int id) ;
+
+}
